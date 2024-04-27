f426d7cfbf9ad23bd6518bcb6027af02fda3368b
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-04-28 01:12:07 +0530

    Time: 0 ms (100%), Space: 9.1 MB (95.2%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0)   return 0;
+        if(n==1)   return nums[0];
+        int ans1 = robber(nums,0,n-2);
+        int ans2 = robber(nums,1,n-1);
+        return max(ans1,ans2);
+    }
+    int robber(vector<int>& nums, int start, int end)
+    {
+        int prev=0;
+        int curr=0;
+        int next=0;
+        for(int i=start;i<=end;i++)
+        {
+            next = max(prev+nums[i],curr);
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+};
