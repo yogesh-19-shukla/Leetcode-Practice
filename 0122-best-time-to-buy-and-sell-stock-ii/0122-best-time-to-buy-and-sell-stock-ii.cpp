2a11401d0fb7c5f1aadc6d2c5d17db0e11dc3765
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-06-08 21:25:38 +0530

    Time: 9 ms (28.01%), Space: 20.5 MB (6.19%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    int f(int n, vector<int>& prices, int ind, int buy, vector<vector<int>>&dp)
+    {
+        //if we have exhausted array we return 0
+        if(ind==n)  return 0;
+        int profit = 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        // max of either we buy first and cannot buy again
+        //or we don't buy first and move to other index for buy
+        if(buy){
+           profit = max(-prices[ind] + f(n,prices,ind+1,0,dp), 0 + f(n,prices,ind+1,1,dp));
+        }
+        // max of either we sell and can buy again 
+        //or we dont sell and move ahead but can't buy again
+        else{
+           profit = max(prices[ind] + f(n,prices,ind+1,1,dp), 0 + f(n,prices,ind+1,0,dp));
+        }
+        return dp[ind][buy] =  profit;
+    }    
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));     // buy can take 2 values - 0/1
+        return f(n,prices,0,1,dp);
+    }
+};
