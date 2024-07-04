85ed6600bc35a7a08a9e55213c2882d23557830f
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-07-04 23:57:27 +0530

    Time: 7 ms (81.34%), Space: 18.3 MB (70.66%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,14 +2,33 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int least = int(n/3) + 1;
         vector<int>ans;
-        map<int,int>mp;
+        int cnt1=0 , cnt2=0 , el1 = INT_MIN , el2 = INT_MIN;
         for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]]==least)    ans.push_back(nums[i]);
-            if(ans.size()==2)  break;
+            if(cnt1==0 && el2!=nums[i]){
+                cnt1++;
+                el1=nums[i];
+            }
+            else if(cnt2==0 && el1!=nums[i]){
+                cnt2++;
+                el2=nums[i];
+            }
+            else if(nums[i]==el1)  cnt1++;
+            else if(nums[i]==el2)  cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }  
         }
-    return ans;      
+        cnt1 = 0, cnt2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==el1) cnt1++;
+            if(nums[i]==el2) cnt2++;
+        }
+        int least = int(n/3) + 1;
+        if(cnt1>=least) ans.push_back(el1); 
+        if(cnt2>=least) ans.push_back(el2);
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
