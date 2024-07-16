c6469fdf9bcb24284747d9cc6292264e18cbac9c
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-07-16 23:23:14 +0530

    Time: 12 ms (5.97%), Space: 16.5 MB (14.83%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        // we will use BFS because in 1 unit time it will take care of same level elements
-        //here DFS will not work 
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-       // will store as {{row,col},time} at t=0 so initially queue is all filled 
         queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -14,38 +11,33 @@ public:
                     q.push({{i,j},0});
                     vis[i][j] = 1;
                 }
-                else {
-                    vis[i][j] = 0;
-                }
             }
         }
-        // now we run our BFS 
-int delrow[] = {-1,0,+1,0};
-             int delcol[] = {0,-1,0,+1};
-             int tm =0;
-        while(!q.empty()){
-             
-             int r = q.front().first.first;
-             int c = q.front().first.second;
-             int time = q.front().second;
-             tm = max(tm,time);
-             q.pop();
-             for(int i=0;i<4;i++)
-             {
+        int tm = 0;
+        int delrow[] = {-1,0,+1,0};
+        int delcol[] = {0,+1,0,-1};
+        while(!q.empty())
+        {
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int time = q.front().second;
+            q.pop();
+           
+            for(int i =0;i<4;i++){
                 int nrow = r + delrow[i];
                 int ncol = c + delcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1&&vis[nrow][ncol]!=1)
-                {
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]!=1){
                     q.push({{nrow,ncol},time+1});
                     vis[nrow][ncol] = 1;
                 }
-             }
+            }
+            tm = max(tm,time);
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]!=1 && grid[i][j]==1)    return -1;
+                if(grid[i][j]==1 && vis[i][j]==0)  return -1;
             }
         }
-       return tm;
+        return tm;
     }
 };
