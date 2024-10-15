00a548da49b43d840fb60012d2ae34847c4ca376
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-10-15 17:00:54 +0530

    Time: 5 ms (81.96%), Space: 8.5 MB (99.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int count[26];
-        if(s.length()!=t.length())  return false;
-        for(int i=0;i<s.length();i++){
+        int count[26] = {0};
+        int n = s.length();
+        int m = t.length();
+        if(n!=m) return false;
+        for(int i=0;i<n;i++){
             count[s[i]-'a']++;
             count[t[i]-'a']--;
         }
-        for(int i=0;i<26;i++){
-            if(count[i]!=0) return false;
+        for(int i=0;i<25;i++){
+            if(count[i]!=0)  return false;
         }
         return true;
     }
