2527e01532e695a205403eeab5c6d2dd752e254e
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-10-21 20:17:52 +0530

    Time: 0 ms (100%), Space: 15.3 MB (60.39%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string> res;
-        generate(n,0,0,"",res);
+        vector<string>res;
+        backtrack(n,0,0,"",res);
         return res;
     }
-    void generate(int n, int o,int c,string s,vector<string>&res)
-    {
-        if(o==n && c==n)  res.push_back(s);
-        if(o<n)   generate(n,o+1,c,s+'(',res);
-        if(c<o)   generate(n,o,c+1,s+')',res);
+
+    void backtrack(int n, int open,int close, string ans, vector<string>&res){
+        if(open==n && close==n){
+            res.push_back(ans);
+        }
+        if(open<n){
+           backtrack(n,open+1,close,ans+'(',res);
+        }
+        if(close<open){
+            backtrack(n,open,close+1,ans+')',res);
+        }
     }
 };
