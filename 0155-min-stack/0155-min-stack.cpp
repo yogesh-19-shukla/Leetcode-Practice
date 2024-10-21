bfe9be17158a0751df89fd4eb9c0c3b9c4305e0f
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-10-21 19:45:01 +0530

    Time: 5 ms (98.13%), Space: 21.5 MB (66.05%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,18 +1,19 @@
 class MinStack {
-    stack<int>s1;
-    stack<int>s2;
 public:
+
+stack<int>s1;
+stack<int>s2;
     MinStack() {
         
     }
     
     void push(int val) {
         s1.push(val);
-        if(s2.empty() || val<=getMin())    s2.push(val);
+        if(s2.empty() or val<=getMin())   s2.push(val);
     }
     
     void pop() {
-        if(s1.top()==getMin())    s2.pop();
+        if(s1.top()==getMin())  s2.pop();
         s1.pop();
     }
     
