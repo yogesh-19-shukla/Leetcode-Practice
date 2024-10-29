11fc9a49d0ac57c88b85946ffa8a939284ecda86
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-10-29 20:31:59 +0530

    Time: 0 ms (100%), Space: 11.7 MB (73.09%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        cost.append(0)
+        for i in range(2,len(cost)):
+            cost[i] += min(cost[i-1],cost[i-2])
+        return cost[len(cost)-1]    
+        
