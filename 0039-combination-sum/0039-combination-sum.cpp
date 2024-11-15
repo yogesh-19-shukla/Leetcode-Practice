d711ec7ec7fe632498ca172051180c727deae89d
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-15 19:57:04 +0530

    Time: 2 ms (62.14%), Space: 12.7 MB (57.11%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
-private:
-    void f(vector<int>& candidates, int target, int ind,vector<vector<int>>&res,vector<int>&temp)
-    {
-        if(ind>=candidates.size()||target<0)  return;
-        if(target==0)  {
-            res.push_back(temp);
-            return;
-        }
-        temp.push_back(candidates[ind]);
-        f(candidates,target-candidates[ind],ind,res,temp);
-        temp.pop_back();
-        f(candidates,target,ind+1,res,temp);
-    }    
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int>temp;
-        f(candidates,target,0,res,temp);
-        return res;
+         vector<vector<int>> ans;
+         vector<int>tmp;
+         f(candidates,target,tmp,ans,0);
+         return ans;
+    }
+
+    void f(vector<int>& candidates, int target,vector<int>& tmp, vector<vector<int>> &ans ,int start)
+    {
+        if(start>=candidates.size() || target<0)  return;
+        if(target==0)
+        {
+            ans.push_back(tmp);
+            return;
+        }
+        tmp.push_back(candidates[start]);
+        f(candidates,target-candidates[start],tmp,ans,start);
+        tmp.pop_back();
+        f(candidates,target,tmp,ans,start+1);
+
     }
 };
