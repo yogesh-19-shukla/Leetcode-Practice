e3cc1284590b1ef95f14663b77a0fb1bded1739c
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-20 14:39:50 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,12 @@
 class Solution {
-private: 
-int f(int i,int j,int m,int n,vector<vector<int>>& dp)
-{
-    if(i==m-1 && j==n-1) return 1;
-    if(i==m || j==n)  return 0;
-    if(dp[i][j]!=-1)  return dp[i][j];
-    else return dp[i][j] = f(i+1,j,m,n,dp) + f(i,j+1,m,n,dp);
-}
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        return f(0,0,m,n,dp);
+        vector<vector<int>>dp(m,vector<int>(n,1));
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
