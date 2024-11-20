12b3f614eab1c20dabc3b90f1e03532ca1bf972e
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-20 21:10:56 +0530

    Time: 0 ms (100%), Space: 14 MB (27.04%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
-         for(int j=0;j<m;j++)
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<vector<int>>dp(row,vector<int>(col,0));
+        for(int i=0;i<col;i++)
         {
-            dp[0][j]=matrix[0][j];    
+            dp[0][i] = matrix[0][i];
         }
-
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                int ld=1e9,rd=1e9;
-                int up=matrix[i][j] + dp[i-1][j];
-    
-                if(j-1>=0)
-                    ld=matrix[i][j] + dp[i-1][j-1];
-                if(j+1<m)
-                    rd=matrix[i][j] + dp[i-1][j+1];
-                
-                dp[i][j] = min(up,min(ld,rd));   
+        for(int i=1;i<row;i++){
+            for(int j=0;j<col;j++){
+                int ld=1e9,rd = 1e9;
+                int up = matrix[i][j] + dp[i-1][j];
+                if(j-1>=0)  ld = matrix[i][j] + dp[i-1][j-1];
+                if(j+1<col) rd = matrix[i][j] + dp[i-1][j+1];
+                dp[i][j] = min(up,min(ld,rd));
             }
         }
-          int mini=dp[n-1][0];
-        
-        for(int j=1;j<m;j++)
-        {
-            mini=min(mini,dp[n-1][j]);
+        int mini = dp[row-1][0];
+        for(int j=0;j<col;j++){
+            mini = min(mini,dp[row-1][j]);
         }
         return mini;
     }
