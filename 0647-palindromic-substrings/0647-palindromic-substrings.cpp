585586b10788b3e66cfa4197d51da8a7df3e22f1
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-20 13:55:51 +0530

    Time: 13 ms (49.86%), Space: 21.7 MB (23.97%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int res = 0;
-        int n= s.length();
+        int n = s.length();
         for(int i=0;i<n;i++)
         {
-            //odd length palindrome
-            middleout(s,i,i,res);
-            //even length palindrome
-            middleout(s,i,i+1,res);
+            f(i,i,s,res);
+            f(i,i+1,s,res);
         }
         return res;
     }
 
-    void middleout(string s,int i,int j,int& res)
+    void f(int i,int j,string s,int &res)
     {
-        while(i>=0 && j<s.size() && s[i]==s[j])
-        {
+        while(i>=0 && j<s.length() && s[i]==s[j]){
             res++;
             i--;
             j++;
