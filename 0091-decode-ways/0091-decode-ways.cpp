f75cd244bf9526a89a036c9bfec9dbed405ccf7d
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-20 22:45:54 +0530

    Time: 0 ms (100%), Space: 8.1 MB (65.47%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        //base case if first digit is 0 return 0
-        if (s[0] == '0') {
-            return 0;
-        }
-        int n = s.size();
-        
-        vector<int> dp(n + 1);
+        int n = s.length();
+        vector<int>dp(n+1,0);
         dp[0] = 1;
-        dp[1] = 1;
-        
-        for (int i = 2; i <= n; i++) {
-            int ones = stoi(s.substr(i - 1, 1));
-            if (ones >= 1 && ones <= 9) {
-                dp[i] += dp[i - 1];
-            }
-            int tens = stoi(s.substr(i - 2, 2));
-            if (tens >= 10 && tens <= 26) {
-                dp[i] += dp[i - 2];
-            }
+        if(s[0]!='0')  dp[1] = 1;
+        else return 0;
+        for(int i=2;i<=n;i++){
+            if(s[i-1]!='0')    dp[i] += dp[i-1];
+            if(s[i-2]=='1' || (s[i-2]=='2' && s[i-1]<='6'))   dp[i] += dp[i-2];
         }
-        
         return dp[n];
     }
 };
