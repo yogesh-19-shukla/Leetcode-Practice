b6f5e88abaf46b408e372887aa649311537de9ee
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-21 22:27:46 +0530

    Time: 150 ms (67.77%), Space: 98.9 MB (39.46%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,23 @@
-//Upvote and Comment :)
-
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int resultant_sums = 0 ;
-        
-        //Adding all the elements of our vector
-        for (auto x : nums)
-            resultant_sums +=x;
-        
-        //Now think that the sum is odd in this special case we can never find two equal partitions
-        if(resultant_sums % 2) return false;
-        //Finding the half off the resultant sum
-        resultant_sums /= 2;
-        
-        //dp vector for storing the results of previous row i.e dp will keep for each number if it has a satisfying subset or not
-        vector<bool> dp(resultant_sums+1,false);
-        dp[0]=true;
-        for(auto x:nums){
-            for(int i=resultant_sums;i>=x;i--){
-                dp[i] = dp[i] || dp[i-x] ;//The "or" operator is used because for each number, either we use it or we don't
-            }
-        }
-        return dp[resultant_sums];
+        int total = 0;
+        int n = nums.size();
+        for(auto i: nums)   total += i;
+        if(total%2!=0)    return false;
+        total= total/2;
+        vector<vector<int>>dp(n,vector<int>(total+1,-1));
+        return f(n-1,total,nums,dp);
+    }
+
+    bool f(int ind,int target,vector<int>& nums,vector<vector<int>>&dp)
+    {
+        if(target==0)  return true;
+        if(ind==0)  return (nums[0]==target);
+        if(dp[ind][target]!=-1)   return dp[ind][target];
+        bool notpick = f(ind-1,target,nums,dp);
+        bool pick = false;
+        if(nums[ind]<=target)   pick = f(ind-1,target-nums[ind],nums,dp);
+        return dp[ind][target] = pick | notpick;
     }
 };
