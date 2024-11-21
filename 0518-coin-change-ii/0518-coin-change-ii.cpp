c203b5cebd4d2b159a84050dcd7f8cabf3f394ea
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-22 00:24:27 +0530

    Time: 15 ms (77.9%), Space: 27 MB (61.88%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<long>> dp(n, vector<long>(amount + 1, -1)); // Create a DP table
-    
-    // Call the utility function to calculate the answer
-    return countWaysToMakeChangeUtil(coins, n - 1, amount, dp);
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return f(n-1,amount,coins,dp);
     }
 
-    long countWaysToMakeChangeUtil(vector<int>& arr, int ind, int T, vector<vector<long>>& dp) {
-    // Base case: if we're at the first element
-    if (ind == 0) {
-        // Check if the target sum is divisible by the first element
-        return (T % arr[0] == 0);
-    }
-    
-    // If the result for this index and target sum is already calculated, return it
-    if (dp[ind][T] != -1)
-        return dp[ind][T];
-        
-    // Calculate the number of ways without taking the current element
-    long notTaken = countWaysToMakeChangeUtil(arr, ind - 1, T, dp);
-    
-    // Calculate the number of ways by taking the current element
-    long taken = 0;
-    if (arr[ind] <= T)
-        taken = countWaysToMakeChangeUtil(arr, ind, T - arr[ind], dp);
-        
-    // Store the sum of ways in the DP table and return it
-    return dp[ind][T] = notTaken + taken;
-}
+    int f(int ind,int x,vector<int>& coins,vector<vector<int>>&dp)
+    {
+        if(ind==0){
+            if(x%coins[0]==0)   return 1;
+            return 0;
+        }
 
+        if(dp[ind][x]!=-1)   return dp[ind][x];
+        int np = f(ind-1,x,coins,dp);
+        int p = 0;
+        if(coins[ind]<=x){
+            p = f(ind,x-coins[ind],coins,dp);
+        }
+        return dp[ind][x] = np + p;
+    }
 };
