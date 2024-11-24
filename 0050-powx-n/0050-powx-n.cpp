56c07bcb87b2f3ec3f56f20d5e28be66f43fb9b1
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-24 19:25:53 +0530

    Time: 0 ms (100%), Space: 8.4 MB (40.67%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long exponent = abs(n);
-        double result = 1.0;
-        
-        for (long i = exponent; i > 0; i /= 2) {
-            if (i % 2 == 1) {
-                result *= x;
+        double res = 1.0;
+        long exp = abs(n);
+        for(long i=exp;i>0;i/=2){
+            if(i%2==1)   {
+                res *= x;
             }
             x *= x;
         }
-        
-        if (n < 0) {
-            return 1.0 / result;
-        }
-        return result;
+
+        if(n<0)  return 1.0/res;
+        return res;
     }
 };
