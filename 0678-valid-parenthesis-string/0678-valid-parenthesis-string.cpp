ef0c747901f338ce380c6ae0c86e7ec4b6d56930
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-24 20:48:14 +0530

    Time: 0 ms (100%), Space: 7.5 MB (84.94%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,31 +1,25 @@
-/*
-make 2 variables cmin,cmax if '(' increase both 
-if ')' decrease both and else min-- , max++;
-at any point if max is -ve return false and if min is -ve make it 0
-return min == 0 ; 
-*/
-
 class Solution {
 public:
     bool checkValidString(string s) {
         int cmin = 0 , cmax = 0;
-        int n = s.size();
-        for(int i=0;i<n;i++){
-            if(s[i]=='(')    {
-                cmin++;  
+        int n = s.length();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='('){
+                cmin++;
                 cmax++;
             }
-            else if(s[i]==')') {
+            else if(s[i]==')'){
                 cmin--;
                 cmax--;
-            } 
-            else {
+            }
+            else{
                 cmin--;
                 cmax++;
             }
-            if(cmax<0)   return false;
-            if(cmin<0)   cmin = 0;
+            if(cmax<0)  return false;
+            if(cmin<0)  cmin=0;
         }
-        return cmin == 0;
+        return cmin==0;
     }
 };
