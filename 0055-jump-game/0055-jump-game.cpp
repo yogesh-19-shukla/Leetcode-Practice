26718c6ff93f0e3f405f323b4ae441bb98860e2e
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-24 19:40:28 +0530

    Time: 0 ms (100%), Space: 51.2 MB (48.36%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
         int reach = 0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.size();i++){
             if(i>reach)  return false;
-        reach = max(reach,i+nums[i]);
+            reach = max(reach,i+nums[i]);
         }
         return true;
     }
