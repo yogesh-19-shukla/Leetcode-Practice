c1930ab75945d02bd0db76c2f7cb8e0dda286c2e
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-25 21:41:42 +0530

    Time: 3 ms (87.89%), Space: 12.6 MB (81.91%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -7,12 +7,11 @@ public:
         return f(n-1,m-1,word1,word2,dp);
     }
 
-    int f(int i,int j,string &word1, string &word2, vector<vector<int>>&dp)
-    {
+    int f(int i,int j,string &word1, string &word2,vector<vector<int>>&dp){
         if(j<0)  return i+1;
         if(i<0)  return j+1;
         if(dp[i][j]!=-1)  return dp[i][j];
         if(word1[i]==word2[j])   return dp[i][j] = f(i-1,j-1,word1,word2,dp);
-        return dp[i][j] = 1 + min(f(i-1,j,word1,word2,dp),min(f(i-1,j-1,word1,word2,dp),f(i,j-1,word1,word2,dp)));
+        else return dp[i][j] = 1 + min(min(f(i-1,j,word1,word2,dp),f(i,j-1,word1,word2,dp)),f(i-1,j-1,word1,word2,dp));
     }
 };
