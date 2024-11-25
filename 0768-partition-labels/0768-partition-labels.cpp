5caa9b7c6bfceb487a01f3ef81dc1fa6d4d47596
Yogesh Shukla <43723598+yogesh-19-shukla@users.noreply.github.com>
2024-11-25 17:00:29 +0530

    Time: 0 ms (100%), Space: 8.1 MB (93.69%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,32 +1,22 @@
-/*
-1. determine last index of each character
-2. make size =0, end =0, vector of res;
-3. for each element increase size and update end 
-4. if i==end push in res , size =0
-TC = O(N)   SC = O(N)
-*/
-
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> lastindex(26);
-        int n = s.size();
-        for(int i=0;i<n;i++)
-        {
+        int n = s.length();
+        vector<int>lastindex(26);
+        for(int i=0;i<n;i++){
             lastindex[s[i]-'a'] = i;
         }
-        int size = 0;
-        int end = 0;
-        vector<int>res;
-        for(int i=0;i<n;i++)
-        {
+
+        int size = 0, end = 0;
+        vector<int>ans;
+        for(int i=0;i<n;i++){
             size++;
             end = max(end,lastindex[s[i]-'a']);
             if(i==end){
-                res.push_back(size);
-                size=0;
+                ans.push_back(size);
+                size = 0;
             }
         }
-        return res;
+        return ans;
     }
 };
